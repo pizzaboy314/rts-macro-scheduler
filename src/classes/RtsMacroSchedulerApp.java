8009eb5bcdc20e636a8e0815eb390e82182e5d62
bryan spahr <bspahr314@gmail.com>
2022-08-17 22:20:15 -0500

    fix layout

--- src/classes/RtsMacroSchedulerApp.java
+++ src/classes/RtsMacroSchedulerApp.java
@@ -3,6 +3,7 @@ package classes;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -43,10 +44,10 @@ public class RtsMacroSchedulerApp extends JPanel implements ActionListener{
 		resultFrameBoundX = (int) (frameSize.width / 2);
 		resultFrameBoundY = (int) (frameSize.height / 2);
 		resultFrameBoundWidth = 500;
-		resultFrameBoundHeight = frameSize.height;
+		resultFrameBoundHeight = 200;
 		resultFrame.setBounds(resultFrameBoundX, resultFrameBoundY, resultFrameBoundWidth, resultFrameBoundHeight);
 		
-		resultFrame.setLayout(new BoxLayout(resultFrame.getContentPane(),BoxLayout.Y_AXIS));
+		resultFrame.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
 		resultFrame.getContentPane().setBackground(new Color(28,31,34));
 		resultFrame.getContentPane().add(new RtsMacroSchedulerRow(20,"1q"), BorderLayout.NORTH);
 		resultFrame.getContentPane().add(new RtsMacroSchedulerRow(35,"9wwww"), BorderLayout.NORTH);
